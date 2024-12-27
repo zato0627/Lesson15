caaaf6fbf45a20e1cd1e1bc3d007af37df8d6155
zato0627 <panmimi38@icloud.com>
2024-12-27 14:19:41 +0900

    Lesson15

---
+++ src/main/java/com/techacademy/TutorialThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialThymeleafApplication.class, args);
+	}
+
+}
